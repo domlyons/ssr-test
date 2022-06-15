b224b95d25a9c8382a68912ca4214fb8e392c8e8
Dom Lyons <dominic.lyons@tfgm.com>
2022-06-15 15:37:09 +0100

    remove dead code

--- app-inner/server.js
+++ app-inner/server.js
@@ -12,20 +12,6 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "./frontend/dist/assets")));
 app.use(cors());
 
-const htmlTemplate = function (root) {
-  return `
-    <div id="app">${root}</div>
-    // <script src="https://localhost:3001/index.js"></script>
-  `;
-};
-
-app.get("/", (req, res) => {
-  const root = ReactDOMServer.renderToString(<App />);
-
-  res.setHeader("Content-Type", "text/html");
-  res.end(htmlTemplate(root));
-});
-
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log("SSR server start at ", PORT);
--- app-main/src/App.js
+++ app-main/src/App.js
@@ -1,23 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
-import * as ReactDOM from "react-dom/client";
 
 function App() {
   const [number, setNumber] = useState(0);
 
-  const [innerHtml, setInnerHtml] = useState(null);
   useEffect(() => {
     const main = async () => {
-      const resp = await fetch("http://localhost:3001");
-      const serverString = await resp.text();
-      console.log(serverString);
-
-      // const newRoot = document.getElementById("newRoot");
-
-      // newRoot.dangerouslySetInnerHTML = { __html: serverString };
-      // root.render(serverString);
-      setInnerHtml(serverString);
       const script = document.createElement("script");
       script.src = "http://localhost:3001/index.js";
 
